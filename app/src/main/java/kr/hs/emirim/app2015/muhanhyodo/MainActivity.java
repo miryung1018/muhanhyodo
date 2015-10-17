6b32ce04d5fbd19f70d63dc65d8e7779ba28efea
Miryung Yeon <miryung1018@naver.com>
2015-10-17 19:32:57 +0900

    #9 GCM PUSH 개념 추가

--- app/src/main/java/kr/hs/emirim/app2015/muhanhyodo/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/app2015/muhanhyodo/MainActivity.java
@@ -1,8 +1,20 @@
 package kr.hs.emirim.app2015.muhanhyodo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,10 +22,8 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.internal.bind.DateTypeAdapter;
-
 import java.sql.Date;
 import java.util.List;
-
 import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
@@ -21,16 +31,103 @@ import retrofit.client.Response;
 import retrofit.converter.GsonConverter;
 
 public class MainActivity extends AppCompatActivity {
-    public static String TAG = "무한효도메인";
+    private static final String TAG = "무한효도메인::MainActivity";
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    private ProgressBar mRegistrationProgressBar;
+    private TextView mInformationTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRegistrationProgressBar = (ProgressBar) findViewById(R.id.registrationProgressBar);
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+                boolean sentToken = sharedPreferences
+                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
+                if (sentToken) {
+                    mInformationTextView.setText(getString(R.string.gcm_send_message));
+                } else {
+                    mInformationTextView.setText(getString(R.string.token_error_message));
+                }
+            }
+        };
+        mInformationTextView = (TextView) findViewById(R.id.informationTextView);
+
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+        getData();
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private void getData() {
         /**
          * Gson 컨버터 이용
          */
@@ -79,7 +176,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Log.i(TAG, "address 가져오기");
+        Log.i(TAG, "medicine 가져오기");
         restAdapter.create(MuhanhyodoService.class).medicine(new Callback<List<Medicine>>() {
             @Override
             public void success(List<Medicine> medicine, Response response) {
@@ -96,6 +193,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Log.i(TAG, "user 가져오기");
         restAdapter.create(MuhanhyodoService.class).user(new Callback<List<User>>() {
             @Override
             public void success(List<User> user, Response response) {
@@ -108,10 +206,11 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void failure(RetrofitError error) {
-                Log.i(TAG, "무슨 영문인지 이해할 수 없음");
+                Log.i(TAG, "무슨 영문인지 이해할 수 없음 ");
             }
         });
 
+        Log.i(TAG, "family 가져오기");
         restAdapter.create(MuhanhyodoService.class).family(new Callback<List<Family>>() {
             @Override
             public void success(List<Family> family, Response response) {
@@ -124,10 +223,11 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void failure(RetrofitError error) {
-                Log.i(TAG, "무슨 영문인지 이해할 수 없음");
+                Log.i(TAG, "무슨 영문인지 이해할 수 없음 ");
             }
         });
 
+        Log.i(TAG, "notice 가져오기");
         restAdapter.create(MuhanhyodoService.class).notice(new Callback<List<Notice>>() {
             @Override
             public void success(List<Notice> notice, Response response) {
@@ -144,6 +244,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Log.i(TAG, "normal 가져오기");
         restAdapter.create(MuhanhyodoService.class).normal(new Callback<List<Normal>>() {
             @Override
             public void success(List<Normal> normal, Response response) {
@@ -159,27 +260,5 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "무슨 영문인지 이해할 수 없음");
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
